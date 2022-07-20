3b1fca7250bb6ac6ed5160d1ae430ae91b45fdea
Barnali Gorai <99495284+barnaligorai@users.noreply.github.com>
2022-07-20 20:52:58 +0530

    Changed npm script for setup.

--- .setup.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-const main = () => {
-  const dotenvFile = '.env';
-  const dotenvFileContent = "COOKIE_NAME='session'\nCOOKIE_KEY='key1'\nSTATIC_DIR='public'";
-  fs.writeFileSync(dotenvFile, dotenvFileContent, 'utf8');
-};
-
-main();
